63e2d5cf3d87931c93b1c38eaef43fdc051ba05d
Matthew Wong <matthew@adveca.com>
2019-11-14 20:49:55 -0800

    learned how to draw basic shapes

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
+import time
 from manimlib.imports import *
 
 class Shapes(Scene):
     def construct(self):
-        circle = Circle()
-        square = Square()
+        circle = Circle(fill_color = GOLD_A, fill_opacity = 1)
+        square = Square(color = PURPLE_A)
+        line = Line(np.array([0,1,0]), np.array([0,-1,0]))
 
         self.play(ShowCreation(square))
         self.play(Transform(square, circle))
+        self.play(FadeIn(line), FadeOut(square)) # FadeOut fades out square, even though it transformed to circle
+        self.play(Rotating(line))
+        time.sleep(2)
+        self.play(FadeOut(line))
