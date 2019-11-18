b1221492640d631ec5d75ee32823ce172844a19d
Matthew Wong <matthew@adveca.com>
2019-11-17 21:40:01 -0800

    implemented system for animating and graphing curves

--- winding.py
+++ winding.py
@@ -3,25 +3,23 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 class Curve:
-    """class for managing space curves"""
-    
     def __init__(self, t_start, t_end, resolution):
-        self.t = np.linspace(t_start, t_end, num = resolution)
+        self.start = t_start
+        self.end = t_end
+        self.t = np.linspace(self.start, self.end, num = resolution)
+        self.windingno = round((self.end - self.start) / (2 * np.pi))
 
     # x, y parametric function
     def x(self, t):
         return np.cos(t)
+
     def y(self, t):
         return np.sin(t)
-    
-    def theta(self, t_value):
-        """angle as vector winds around reference point"""
-        self.theta = self.t[t_value]
-        return self.theta
 
-    def winding(self, start, end):
-        return round((self.theta(end), self.theta(start)) / (2 * np.pi))
-
+    def winding(self, t):
+        spiral_x = np.cos(self.t[:t.astype(int)])
+        spiral_y = np.sin(self.t[:t.astype(int)])
+        return (spiral_x, spiral_y)
 
 X_MIN, X_MAX = -2, 2
 Y_MIN, Y_MAX = -2, 2
@@ -31,8 +29,8 @@ REF_X, REF_Y = 0, 0
 FPS = 60
 
 fig, ax = plt.subplots()
+curve = Curve(START, END, RESOLUTION)
 
-# set up graph axes
 def set_axes(x_min, x_max, y_min, y_max):
     plt.axis([x_min, x_max, y_min, y_max])
     plt.axis("scaled")
@@ -44,14 +42,13 @@ def set_axes(x_min, x_max, y_min, y_max):
     ax.set_xticks([])
     ax.set_yticks([])
 
-curve = Curve(START, END, RESOLUTION)
-ax.plot(curve.x(curve.t), curve.y(curve.t))
-
 def animate(i):
     ax.clear()
     ax.plot(curve.x(curve.t), curve.y(curve.t))
     ax.plot([REF_X, curve.x(i)], [REF_Y, curve.y(i)])
+    ax.plot(curve.winding(i)[0], curve.winding(i)[1])
     set_axes(X_MIN, X_MAX, Y_MIN, Y_MAX)
 
-ani = animation.FuncAnimation(fig, animate, frames = np.linspace(START, END), interval = 1000 / FPS)
+ani = animation.FuncAnimation(fig, animate, frames = np.linspace(START, END, 100), interval = 1000 / FPS, repeat = True)
 plt.show()
+print(curve.windingno)
