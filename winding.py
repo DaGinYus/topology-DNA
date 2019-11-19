b34d4e2e1ca5bf70accab11a4baa4a8dd6726992
Matthew Wong <matthew@adveca.com>
2019-11-19 11:45:01 -0800

    tried implementing different angle calculation

--- winding.py
+++ winding.py
@@ -7,7 +7,6 @@ class Curve:
         self.start = t_start
         self.end = t_end
         self.t = np.linspace(self.start, self.end, num = resolution)
-        self.windingno = 0
         self.theta = 0
         self.spiral_x = []
         self.spiral_y = []
@@ -20,57 +19,84 @@ class Curve:
         return np.sin(t)
 
     def vector(self, t):
-        """returns vector from reference point to curve at time t"""
+        """Returns vector from reference point to curve at time t."""
         self.v = [self.x(t) - REF_X, self.y(t) - REF_Y]
-        return self.v[0], self.v[1]
 
-    def get_theta(self, t):
-        """calculates angle between vector and horizontal unit vector"""
+    def angle(self, t):
+        """Calculates angle between vector and horizontal unit vector."""
         self.vector(t)
         v_mag = np.sqrt(self.v[0] ** 2 + self.v[1] ** 2)
         v_hat = np.divide(self.v, v_mag)
-        unit_v = [REF_X + 1, REF_Y]
-        dot = np.vdot(unit_v, v_hat) # cos
-        cross = np.cross(unit_v, v_hat) # sin
+        v_base = [1, 0]
+        dot = np.vdot(v_base, v_hat) # cos
+        cross = np.cross(v_base, v_hat) # sin
         angle = np.arctan2(cross, dot)
-
-        # convert angle to positive if negative
+        # convert angle if negative
         if angle < 0:
-            angle += 2 * np.pi
+            angle += np.pi
         return angle
 
-    def count_winds(self, t):
-        """checks if vector has rotated past origin and updates windingno"""
-        TIMESTEP = 0.2
-        adjustment = self.windingno * 2 * np.pi
-        self.theta = self.get_theta(t) + self.windingno * 2 * np.pi
-        theta_future = self.get_theta(t + TIMESTEP) + self.windingno * 2 * np.pi # theta gets reset to 0 when it passes origin
-        if theta_future < self.theta:
-            self.windingno += 1
+    def sum_angle(self, t):
+        """Sums changes in angle to calculate theta.
+           It is implemented this way so it can handle winding and unwinding.
+           Make sure this is only called ONCE"""
+        TIMESTEP = (self.end - self.start) / RESOLUTION
+
+        theta1, theta2 = self.angle(t), self.angle(t + TIMESTEP)
+        delta = theta2 - theta1
+        print(delta)
+        self.theta += delta
+
+    def windingno(self):
+        """Uses winding number formula (theta(b) - theta(a)) / 2pi to update winding number.
+           Assume that theta_0 = 0."""
+        return np.floor(self.theta / (2 * np.pi))
+
+    def spiral(self, t):
+        """Graphs a spiral visualization of the winding number."""
+        self.sum_angle(t)
+        r = 0.02 * self.theta # scaling factor for spiral
+        self.spiral_x.append(r * np.cos(self.angle(t)) + REF_X)
+        self.spiral_y.append(r * np.sin(self.angle(t)) + REF_Y)
+
+    # def count_winds(self, t):
+    #     """checks if vector has rotated past initial point and updates windingno"""
+    #     TIMESTEP = (self.end - self.start) / RESOLUTION
+        
+    #     adjustment = self.windingno * 2 * np.pi
+    #     self.theta = self.get_theta(t) - self.theta_0
+    #     theta_future = self.get_theta(t + TIMESTEP) - self.theta_0
+
+    #     self.theta += adjustment
+    #     theta_future += adjustment
+
+    #     if theta_future < self.theta:
+    #         self.windingno += 1
     
-    def winding(self, t):
-        """draws spiral representation of winding around reference point"""
-        self.count_winds(t)
-        r = 0.02 * self.theta
-        self.spiral_x.append(r * np.cos(self.theta))
-        self.spiral_y.append(r * np.sin(self.theta))
-        return self.spiral_x, self.spiral_y
+    # def winding(self, t):
+    #     """draws spiral representation of winding around reference point"""
+    #     self.count_winds(t)
+    #     r = 0.02 * (self.theta - self.theta_0)
+    #     print(self.theta)
+    #     self.spiral_x.append(r * np.cos(self.theta + self.theta_0) + REF_X)
+    #     self.spiral_y.append(r * np.sin(self.theta + self.theta_0) + REF_Y)
+    #     return self.spiral_x, self.spiral_y
 
     
 X_MIN, X_MAX = -2, 2
 Y_MIN, Y_MAX = -2, 2
 RESOLUTION = 200
-START, END = 0, 4 * np.pi
-REF_X, REF_Y = 0, 0
+START, END = 0, 2 * np.pi
+REF_X, REF_Y = -0, 0
 FPS = 60
-STEP = 50
 
 fig, ax = plt.subplots()
 curve = Curve(START, END, RESOLUTION)
 
 def set_axes(x_min, x_max, y_min, y_max):
-    plt.axis([x_min, x_max, y_min, y_max])
     plt.axis("scaled")
+    ax.set_xlim([x_min, x_max])
+    ax.set_ylim([y_min, y_max])
     ax.spines["left"].set_position("center")
     ax.spines["top"].set_position("center")
     ax.spines["right"].set_visible(False)
@@ -83,13 +109,13 @@ def animate(i):
     ax.clear()
     ax.plot(curve.x(curve.t), curve.y(curve.t))
     # plot vector
-    vector_x, vector_y = curve.vector(i)
-    ax.plot([REF_X, vector_x], [REF_Y, vector_y])
+    ax.plot([REF_X, curve.x(i)], [REF_Y, curve.y(i)])
     # plot spiral
-    spiral_x, spiral_y = curve.winding(i)
-    ax.plot(spiral_x, spiral_y)
-    ax.text(X_MIN + 0.5, Y_MAX - 1, "winding number = %d" %curve.windingno, horizontalalignment = "left", verticalalignment = "bottom")
+    curve.spiral(i)
+    ax.plot(curve.spiral_x, curve.spiral_y)
+    # display winding number in real time
+    ax.text(X_MIN, Y_MAX, "winding number = %d" %curve.windingno(), horizontalalignment = "left", verticalalignment = "bottom")
     set_axes(X_MIN, X_MAX, Y_MIN, Y_MAX)
 
-ani = animation.FuncAnimation(fig, animate, frames = np.linspace(START, END, STEP), interval = 1000 / FPS, repeat = False)
+ani = animation.FuncAnimation(fig, animate, frames = np.linspace(START, END, RESOLUTION), interval = 1000 / FPS, repeat = False)
 plt.show()
